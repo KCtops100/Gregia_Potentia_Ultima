90e74d857e5bf071935317ffe10cdeda659c29ff
KCtops <126096649+KCtops@users.noreply.github.com>
2024-08-28 17:24:51 -0400

    added refined storage.

---
+++ kubejs/client_scripts/hiding_from_JEI.js
@@ -0,0 +1,6 @@
+JEIEvents.hideItems((event) => {
+    event.hide('refinedstorage:silicon');
+    event.hide('refinedstorage:raw_basic_processor');
+    event.hide('refinedstorage:raw_improved_processor');
+    event.hide('refinedstorage:raw_advanced_processor');
+});
---
+++ kubejs/startup_scripts/refined_advanced_processor.js
@@ -0,0 +1,3 @@
+StartupEvents.registry('item', event => {
+    event.create('refined_advanced_processor')
+})
