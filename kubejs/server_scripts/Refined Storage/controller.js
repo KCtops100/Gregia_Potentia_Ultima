0ce7ffe6c3db17f60a85525ec33f01e202f23893
KCtops <126096649+KCtops@users.noreply.github.com>
2024-08-29 09:41:32 -0400

    added controller.js.

---
+++ kubejs/server_scripts/Refined Storage/controller.js
@@ -0,0 +1,32 @@
+ServerEvents.recipes(event => {
+    event.recipes.gtceu.assembler('refinedstorage:controller1')
+        .itemInputs(
+            'gtceu:lv_machine_hull',
+            '4x gtceu:steel_plate',
+            'kubejs:refined_advanced_processor',
+            '3x refinedstorage:cable'
+        )
+        .inputFluids(
+            Fluid.of('gtceu:soldering_alloy', 72)
+        )
+        .itemOutputs(
+            'refinedstorage:controller'
+        )
+        .duration(20 * 60)
+        .EUt(32)
+    event.recipes.gtceu.assembler('refinedstorage:controller2')
+        .itemInputs(
+            'gtceu:lv_machine_hull',
+            '4x gtceu:steel_plate',
+            'kubejs:refined_advanced_processor',
+            '3x refinedstorage:cable'
+        )
+        .inputFluids(
+            Fluid.of('gtceu:tin', 144)
+        )
+        .itemOutputs(
+            'refinedstorage:controller'
+        )
+        .duration(20 * 60)
+        .EUt(32)
+})
