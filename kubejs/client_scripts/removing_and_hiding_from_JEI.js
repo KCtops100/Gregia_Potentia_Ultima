a4ff6ebec281a3bd8148df221e30ada02f61f1b4
KCtops <126096649+KCtops@users.noreply.github.com>
2024-08-29 09:34:58 -0400

    changed and modified removing_and_hiding_from_JEI.js (formerly hiding_from_JEI.js).

---
+++ kubejs/client_scripts/removing_and_hiding_from_JEI.js
@@ -0,0 +1,20 @@
+JEIEvents.hideItems((event) => {
+    const itemsToHide = [
+        'refinedstorage:silicon',
+        'refinedstorage:raw_basic_processor',
+        'refinedstorage:raw_improved_processor',
+        'refinedstorage:raw_advanced_processor',
+        'refinedstorage:processor_binding',
+        'refinedstorage:basic_processor',
+        'refinedstorage:improved_processor',
+        'refinedstorage:advanced_processor',
+        'refinedstorage:machine_casing',
+        'refinedstorage:quartz_enriched_iron',
+        'refinedstorage:quartz_enriched_iron_block',
+    ];
+
+    itemsToHide.forEach((item) => {
+        event.remove({ output: item });
+        event.hide(item);
+    });
+});
