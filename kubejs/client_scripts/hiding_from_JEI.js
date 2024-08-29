ff76173eb9d66dae476bbe7e27b5b4fdd29cac89
KCtops <126096649+KCtops@users.noreply.github.com>
2024-08-29 09:38:10 -0400

    deleted hiding_from_JEI.js.

--- kubejs/client_scripts/hiding_from_JEI.js
+++
@@ -1,9 +0,0 @@
-JEIEvents.hideItems((event) => {
-    event.hide('refinedstorage:silicon');
-    event.hide('refinedstorage:raw_basic_processor');
-    event.hide('refinedstorage:raw_improved_processor');
-    event.hide('refinedstorage:raw_advanced_processor');
-    event.hide('refinedstorage:processor_binding');
-    event.hide('refinedstorage:basic_processor');
-    event.hide('refinedstorage:improved_processor')
-});
