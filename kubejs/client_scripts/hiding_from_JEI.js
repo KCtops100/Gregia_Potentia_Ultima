254023f60ce8e78cff9cc9d342f62aa6c3e8117b
KCtops <126096649+KCtops@users.noreply.github.com>
2024-08-29 09:12:08 -0400

    added hiding_from_JEI.js

--- kubejs/client_scripts/hiding_from_JEI.js
+++ kubejs/client_scripts/hiding_from_JEI.js
@@ -3,4 +3,7 @@ JEIEvents.hideItems((event) => {
     event.hide('refinedstorage:raw_basic_processor');
     event.hide('refinedstorage:raw_improved_processor');
     event.hide('refinedstorage:raw_advanced_processor');
+    event.hide('refinedstorage:processor_binding');
+    event.hide('refinedstorage:basic_processor');
+    event.hide('refinedstorage:improved_processor')
 });
