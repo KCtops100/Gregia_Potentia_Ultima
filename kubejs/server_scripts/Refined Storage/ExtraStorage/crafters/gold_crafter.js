81a608804866b65000d8838fc7d1f82c26362622
KCtops <126096649+KCtops@users.noreply.github.com>
2024-09-03 19:34:04 -0400

    added gold_crafter.js. modified iron_crafter.js.

--- kubejs/server_scripts/Refined Storage/ExtraStorage/crafters/gold_crafter.js
+++ kubejs/server_scripts/Refined Storage/ExtraStorage/crafters/gold_crafter.js
@@ -1,34 +1,34 @@
 ServerEvents.recipes(event => {
-    event.remove({ output: 'extrastorage:iron_crafter' })
-    event.recipes.gtceu.assembler('extrastorage:iron_crafter1')
+    event.remove({ output: 'extrastorage:gold_crafter' })
+    event.recipes.gtceu.assembler('extrastorage:gold_crafter1')
         .itemInputs(
             '2x kubejs:refined_neural_processor',
-            '3x refinedstorage:crafter',
+            '5x refinedstorage:crafter',
             'gtceu:hv_machine_hull',
             '#gtceu:circuits/hv',
-            '4x gtceu:stainless_steel_plate'
+            '4x gtceu:titanium_plate'
         )
         .inputFluids(
             Fluid.of('gtceu:soldering_alloy', 72)
         )
         .itemOutputs(
-            'extrastorage:iron_crafter'
+            'extrastorage:gold_crafter'
         )
         .duration(20 * 60)
         .EUt(32)
-    event.recipes.gtceu.assembler('extrastorage:iron_crafter2')
+    event.recipes.gtceu.assembler('extrastorage:gold_crafter2')
         .itemInputs(
             '2x kubejs:refined_neural_processor',
-            '3x refinedstorage:crafter',
-            'gtceu:hv_machine_hull',
-            '#gtceu:circuits/hv',
-            '4x gtceu:stainless_steel_plate'
+            '5x refinedstorage:crafter',
+            'gtceu:ev_machine_hull',
+            '#gtceu:circuits/ev',
+            '4x gtceu:titanium_plate'
         )
         .inputFluids(
             Fluid.of('gtceu:tin', 144)
         )
         .itemOutputs(
-            'extrastorage:iron_crafter'
+            'extrastorage:gold_crafter'
         )
         .duration(20 * 60)
         .EUt(32)
